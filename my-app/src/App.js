587e913717bc0b7a1ee784cdd7b3b6df400b58f6
YoungHeaKim <dydgo9507@gmail.com>
2017-11-13 17:02:16 +0900

    movies를 state안으로 추가 시키고 state안에 있는 movie를 추가시키는 작업을 하였다.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,25 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie'
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "http://cfile230.uf.daum.net/image/120165194C241D693330B4"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://resizing.flixster.com/G-2niPIShTNblY9-sUj9FrQit3U=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NTg5OzEyMDA7ODAwOzEyMDA"
-  },
-  {
-    title: "Oldboy",
-    poster:"https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg" 
-  },
-  {
-    title: "Star Wars",
-    poster: "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
-  },
-]
-
 class App extends Component {
 
   // Render: componentWillMount() -> render() -> componentDidMount()
@@ -28,24 +9,49 @@ class App extends Component {
 
   // render될 동안의 setTimeOut으로 render되는 동안 지연시간을 주기
   state = {
-    greeting: 'Hello!'
+    greeting: 'Hello!',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "http://cfile230.uf.daum.net/image/120165194C241D693330B4"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://resizing.flixster.com/G-2niPIShTNblY9-sUj9FrQit3U=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NTg5OzEyMDA7ODAwOzEyMDA"
+      },
+      {
+        title: "Oldboy",
+        poster:"https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg" 
+      },
+      {
+        title: "Star Wars",
+        poster: "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
+      },
+    ]
   }
 
   // state를 바꿀 때에는 setState를 설정해주어야 한다.
   componentDidMount() {
     setTimeout(() => {
+      //새로운 무비를 추가하는 법, 이방법을 사용하면 인피니티 스크롤을 사용할 수 있다.
+      // 만약 ...this.state.movies를 뒤에 사용하면 새로추가할 객체가 맨위에 추가되고 그 다음에 위에의 값들이 작성이 된다.
       this.setState({
-        greeting: 'Hello again!'
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Transpotting",
+            poster: "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
+          }
+        ]
       })
-    }, 2000)
+    }, 5000)
   }
 
   render() {
     return (
       // jsx
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}/>
         })}
       </div>

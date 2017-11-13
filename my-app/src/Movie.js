c80cc31ffbb82436d24984b64fd578c7360151af
YoungHeaKim <dydgo9507@gmail.com>
2017-11-13 16:24:14 +0900

    Movie 리스트를 만드는 코드 작성
    Movie 리스트에서 poster와 제목을 넣어주는 코드를 작성하였다. 이것을 컴포넌트화 하여 작성

---
+++ my-app/src/Movie.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+class Movie extends Component {
+  // title과 poster 값을 숫자나 true or false로 반환되는 되지 않고 string으로 출력하는 코드
+  // isRequired를 사용하면 어떤것이 빠졌는지에 대해 콘솔에서 보여준다.
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  }
+
+  // component lifecycle
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <MoviePoster poster={this.props.poster} />
+        <h1>{this.props.title}</h1>
+      </div>
+    )
+  }
+}
+
+class MoviePoster extends Component {
+
+  static propTypes = {
+    poster: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <img src={this.props.poster}/>
+    )
+  }
+}
+
+export default Movie

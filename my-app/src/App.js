b87746888defa840ebc76ea5a729df4404e7461f
YoungHeaKim <dydgo9507@gmail.com>
2017-11-14 17:12:41 +0900

    Loading state에 대해 작성하는 코드
    state에 있는 것들을 setTimeOut으로 넣어두고 새로운 함수를 만들어 거기에 map 함수를 돌려서 정보들을 가져와서 쓰고 Loading되는 시간에 Loading이라는 메세지를 보옂주어 로딩이 끝나면 정보를 불러오는 코드를 작성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,25 +9,6 @@ class App extends Component {
 
   // render될 동안의 setTimeOut으로 render되는 동안 지연시간을 주기
   state = {
-    greeting: 'Hello!',
-    movies: [
-      {
-        title: "Matrix",
-        poster: "http://cfile230.uf.daum.net/image/120165194C241D693330B4"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: "https://resizing.flixster.com/G-2niPIShTNblY9-sUj9FrQit3U=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NTg5OzEyMDA7ODAwOzEyMDA"
-      },
-      {
-        title: "Oldboy",
-        poster:"https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg" 
-      },
-      {
-        title: "Star Wars",
-        poster: "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
-      },
-    ]
   }
 
   // state를 바꿀 때에는 setState를 설정해주어야 한다.
@@ -37,7 +18,22 @@ class App extends Component {
       // 만약 ...this.state.movies를 뒤에 사용하면 새로추가할 객체가 맨위에 추가되고 그 다음에 위에의 값들이 작성이 된다.
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "Matrix",
+            poster: "http://cfile230.uf.daum.net/image/120165194C241D693330B4"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://resizing.flixster.com/G-2niPIShTNblY9-sUj9FrQit3U=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NTg5OzEyMDA7ODAwOzEyMDA"
+          },
+          {
+            title: "Oldboy",
+            poster:"https://www.languagetrainers.com/reviews/foreign-film-reviews/uploads/9214-Oldboy.jpg" 
+          },
+          {
+            title: "Star Wars",
+            poster: "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
+          },
           {
             title: "Transpotting",
             poster: "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2017/10/the-last-jedi-theatrical-blog.jpg"
@@ -47,13 +43,21 @@ class App extends Component {
     }, 5000)
   }
 
+  // movies라는 variable 데이터를 저장하는 코드
+  // _를 붙인 이유는 리엑트 자체 기능과 내가 만든 기능의 차이를 두기 위해 내가 만든 기능 이름앞에 _를 붙인다.
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
+    })
+    return movies
+  }
+
   render() {
     return (
       // jsx
+      // 3항연산자를 사용하여 성공하면 위의 함수를 실행해준다.
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }

9fe608440e8e2c69351994b79f03c86386ff8de5
YoungHeaKim <dydgo9507@gmail.com>
2017-11-26 17:22:21 +0900

    기본적인 reducer를 작성

---
+++ tomato-timer/reducer.js
@@ -0,0 +1,57 @@
+// 필요한 것을 불러온다.
+// Import
+
+// 액션을 정의한다.
+// Actions
+
+const START_TIMER = 'START_TIMER';
+const RESTART_TIMER = 'RESTART_TIMER';
+const ADD_SECOND = 'ADD_SECOND';
+
+// 액션의 creator를 정의
+// Actions creators
+
+function startTimer() {
+  return {
+    type: START_TIMER
+  }
+}
+function restartTimer() {
+  return {
+    type: RESTART_TIMER
+  }
+}
+function addSecond() {
+  return {
+    type: ADD_SECOND
+  }
+}
+
+// 리듀서 정의
+// Reducer
+
+const TIME_DURATION = 1500;
+
+const initialState = {
+  isPlaying: false,
+  elapsedTime: 0,
+  timerDuration: TIME_DURATION
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type){
+    case START_TIMER:
+      return applyStartTimer(state);
+    case RESTART_TIMER:
+      return applyRestartTimer(state);
+    case ADD_SECOND:
+      return applyAddSecond(stete);
+  }
+}
+
+// Reducer Function 생성
+// Reducer Function
+
+// Export Action Creators
+
+// Export Reducer

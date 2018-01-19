374aea9374fa7790357d45830f7dc1fec58cd1df
younghea <dydgo9507@gmail.com>
2018-01-19 16:38:34 +0900

    todoItem을 추가

--- todos/src/components/TodoItemList.js
+++ todos/src/components/TodoItemList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './css/TodoItem.css';
+import TodoItem from "./TodoItem";
 
 class TodoItemList extends Component {
   render() {
@@ -7,8 +7,12 @@ class TodoItemList extends Component {
 
     return (
       <div>
-
+        <TodoItem text="안녕"/>
+        <TodoItem text="리액트"/>
+        <TodoItem text="반가워"/>
       </div>
     )
   }
 }
+
+export default TodoItemList;

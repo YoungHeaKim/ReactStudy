820aeffcac794df1384c9df5b5f4ec006ab71a97
younghea <dydgo9507@gmail.com>
2018-01-19 16:26:15 +0900

    TodoItem에 관한 components를 생성
    props로 3개를 주었다.
    todos는 todo 객체들이 들어있는 배열
    onToggle는 체크박스를 키고 끄는 함수
    onRemove는 아이템을 삭제시키는 함수

---
+++ todos/src/components/TodoItemList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './css/TodoItem.css';
+
+class TodoItemList extends Component {
+  render() {
+    const {todos, onToggle, onRemove } = this.props;
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}

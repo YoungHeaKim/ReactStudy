e9ae295c8d019436a253b3e3ada6b8164493c9ee
younghea <dydgo9507@gmail.com>
2018-02-12 17:15:36 +0900

    form을 사용하여 입력을 하였을 때 todoItemList에 추가가 되도록 코딩을 해줌

--- todos/src/components/TodoItemList.js
+++ todos/src/components/TodoItemList.js
@@ -4,14 +4,25 @@ import TodoItem from "./TodoItem";
 class TodoItemList extends Component {
   render() {
     const {todos, onToggle, onRemove } = this.props;
+    
+    const todoList = todos.map(
+      ({id, text, checked}) => (
+        <TodoItem
+          id={id}
+          text={text}
+          checked={checked}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          key={id}
+        />
+      )
+    );
 
     return (
       <div>
-        <TodoItem text="안녕"/>
-        <TodoItem text="리액트"/>
-        <TodoItem text="반가워"/>
+        {todoList}
       </div>
-    )
+    );
   }
 }
 

f310726476de4642c1ab87c28524cbb5111c0e30
younghea <dydgo9507@gmail.com>
2018-02-12 17:01:12 +0900

    form 태그가 작동하게 만들었다.

--- todos/src/App.js
+++ todos/src/App.js
@@ -17,9 +17,47 @@ class App extends Component {
     ]
   }
 
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: '',
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
   render() {
+    const { input } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
+
     return (
-      <TodoListTemplate form={<Form />}>
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
         <TodoItemList />
       </TodoListTemplate>
     );

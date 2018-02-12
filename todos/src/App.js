7b8606d477d681187ab579a5e5e96f1b3b8653d1
younghea <dydgo9507@gmail.com>
2018-02-12 16:55:58 +0900

    기본 state를 정의하여 주었다.

--- todos/src/App.js
+++ todos/src/App.js
@@ -3,11 +3,24 @@ import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
+
 class App extends Component {
+
+  id = 3
+
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: ' 리액트 소개', checked: false },
+      { id: 1, text: ' 리액트 소개', checked: true },
+      { id: 2, text: ' 리액트 소개', checked: false }
+    ]
+  }
+
   render() {
     return (
-      <TodoListTemplate form={<Form/>}>
-        <TodoItemList/>
+      <TodoListTemplate form={<Form />}>
+        <TodoItemList />
       </TodoListTemplate>
     );
   }

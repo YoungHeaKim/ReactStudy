5530a0b094bb78acd4b72c7ab25bb09ff50657e7
younghea <dydgo9507@gmail.com>
2018-02-18 17:04:38 +0900

    해당 todolist에 맞는 id값이 들어가있는 확인하기 위해 console창에 id 값을 띄워주는 코드를 작성

--- todos/src/components/TodoItem.js
+++ todos/src/components/TodoItem.js
@@ -5,6 +5,8 @@ class TodoItem extends Component {
   render() {
     const { text, checked, id, onToggle, onRemove } = this.props;
 
+    console.log(id);
+
     return (
       <div className="todo-item" onClick={() => onToggle(id)}>
         <div className="remove" onClick={(e) => {
